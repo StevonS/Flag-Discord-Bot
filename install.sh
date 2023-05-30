4b89e64f86cd67fca140a6d3fd75b877bf4f2175
StevonS <110857291+StevonS@users.noreply.github.com>
2023-05-30 07:56:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt install unzip && sudo apt install pip && sudo apt install python3 && sudo apt update && sudo apt upgrade
 pip install cairosvg
+pip install libcairo2
 pip install py-cord
 unzip Flags.zip
 rm Flags.zip
